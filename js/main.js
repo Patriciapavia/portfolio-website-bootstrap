e9e3fb9a4ec1b55fbf4fc2faf3475b997f870dc9
patricia <patriciapavia236@gmail.com>
2020-09-14 23:15:56 +1000

    created client section

--- js/main.js
+++ js/main.js
@@ -36,4 +36,22 @@ $(document).ready(function () {
       },
     },
   });
+  // sticky nav menu
+
+  let nav_offset_top = $(".header_area").height() + 50;
+
+  function navbarFixed() {
+    if ($(".header_area").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $(".header_area .main-menu").addClass("navbar-fixed");
+        } else {
+          $(".header_area .main-menu").removeClass("navbar-fixed");
+        }
+      });
+    }
+  }
+
+  navbarFixed();
 });
